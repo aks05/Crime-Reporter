0dc86a594bf2c9ac52eb2c5489ba0a0c0ef1d01a
aks 05 <ayushthakur05102000@gmail.com>
2019-07-06 14:26:45 +0530

    Feature of Map is added

--- app/src/main/java/com/example/adsadf/SpecificCrimeActivity.java
+++ app/src/main/java/com/example/adsadf/SpecificCrimeActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SpecificCrimeActivity extends AppCompatActivity {
+    String[] crimeData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +20,36 @@ public class SpecificCrimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_specific_crime);
 
         Intent intent= getIntent();
-        String[] crimeData= intent.getStringArrayExtra("crime");
+        crimeData= intent.getStringArrayExtra("crime");
 
         FragmentTransaction transaction = this.getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.asc_ll, new SpecificCrimeFragment(crimeData));
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater menuInflater= getMenuInflater();
+        menuInflater.inflate(R.menu.asc_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (item.getItemId()== R.id.asc_show_map) {
+            if (crimeData[4].equals("No Latitude Found") ||
+                    crimeData[5].equals("No Longitude Found")) {
+                Toast.makeText(this, "No Location Found", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                Intent intent= new Intent(android.content.Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:"+crimeData[4]+","+crimeData[5]));
+                Intent chooser= Intent.createChooser(intent, "Launch Maps");
+                startActivity(chooser);
+            }
+            return true;
+        }
+        else return super.onOptionsItemSelected(item);
     }
 }
